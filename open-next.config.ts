3e29386ef94f0424679a7bfeea0e1962b0ee7b30
lj <li@qq.com>
2026-04-23 22:13:41 +0800

    optimize demand

--- open-next.config.ts
+++ open-next.config.ts
@@ -2,6 +2,7 @@ import { defineCloudflareConfig } from "@opennextjs/cloudflare/config"
 import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache"
 import { withRegionalCache } from "@opennextjs/cloudflare/overrides/incremental-cache/regional-cache"
 import d1NextTagCache from "@opennextjs/cloudflare/overrides/tag-cache/d1-next-tag-cache"
+import purgeCache from "@opennextjs/cloudflare/overrides/cache-purge"
 
 export default defineCloudflareConfig({
   incrementalCache: withRegionalCache(r2IncrementalCache, {
@@ -9,4 +10,5 @@ export default defineCloudflareConfig({
     defaultLongLivedTtlSec: 604800, // 7 days
   }),
   tagCache: d1NextTagCache,
+  cachePurge: purgeCache(),
 })
